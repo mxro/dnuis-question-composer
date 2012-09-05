f27aff97f75cc10d0e8bc6ef690f35fb1140961a
commit <commit@noemail.com>
2012-09-05 19:17:05 +1200

    added first simple implementation of question data module

--- 0.0.1/strategy-question-data-js-0.0.1.js
+++ 0.0.1/strategy-question-data-js-0.0.1.js
@@ -1,32 +1,106 @@
 // <!-- one.upload https://u1.linnk.it/qc8sbw/usr/apps/textsync/docs/strategy-question-data-js-0.0.1 -->
 
-
 (function($, AJ) {
 
-		$.initStrategyQuestionData = function(params) {
+	$.initStrategyQuestionData = function(params) {
 
-			var client = params.client;
-			
-			var qd = {};
-			
-			qd.submitQuestion(data, onSuccess) {
-				
-				client.seed({
-					onSuccess: function(res) {
-						
-					},
-					onFailure: function(ex) {
-						AJ.ui.notify(ex, "alert-error");
-					}
-				};
-				
-			};
-			
-			
-			return {
-				submitQuestion: qd.submitQuestion
-			};
+		// final parameters
+		var client = params.client;
+
+		// constants
+		var aStrategyQuestion = client
+				.reference("http://slicnet.com/mxrogm/mxrogm/apps/nodejump/docs/8/n/Types/Strategy_Quadrant_Questi");
+		var aBrandName = client.reference("http://slicnet.com/mxrogm/mxrogm/apps/nodejump/docs/8/n/Types/Brand_Name");
+		var aBrandImage = client.reference("http://slicnet.com/mxrogm/mxrogm/apps/nodejump/docs/8/n/Types/Brand_Image");
+		var aCorrectStrategy = client.reference("http://slicnet.com/mxrogm/mxrogm/apps/nodejump/docs/8/n/Types/Correct_Strategy");
+		
+		var qd = {};
+
+		qd.submitQuestion = function(data, onSuccess) {
+
+			qd.priv.prepareSeedNodeForQuestion(data, onSuccess);
+
 		};
 
-	})(jQuery, AJ);
+		qd.priv = {};
+
+		qd.priv.prepareSeedNodeForQuestion = function(data, onSuccess) {
+			client.seed({
+				onSuccess : function(res) {
+
+					client.appendSafe({
+						node : "q",
+						atAddress : "./q",
+						to : res.seedNode,
+						onSuccess : function(ar) {
+							qd.priv.writeQuestionDataToNode(ar.appendedNode, res.secret,
+									data, onSuccess);
+						},
+						onFailure : function(ex) {
+							AJ.ui.notify(ex, "alert-error");
+						}
+					});
+
+				},
+				onFailure : function(ex) {
+					AJ.ui.notify(ex, "alert-error");
+				}
+			});
+		};
+
+		qd.priv.writeQuestionDataToNode = function(node, secret, data, onSuccess) {
+			client.append({
+				node : aStrategyQuestion,
+				to : node
+			});
+			
+			var brandName = client.append({
+				node: data.brandName,
+				to: node,
+				atAddress: "./brandName"
+			});
+			
+			client.append({
+				node: aBrandName,
+				to: brandName
+			});
+			
+			var imageLink = client.append({
+				node: data.imageLink,
+				to: node,
+				atAddress: "./brandImageLink"
+			});
+			
+			client.append({
+				node: aBrandImage,
+				to: imageLink
+			});
+			
+			var correctStrategy = client.append({
+				node: data.correctStrategy,
+				to: node,
+				atAddress: "./correctStrategy"
+			});
+			
+			
+			client.append({
+				node: aCorrectStrategy,
+				to: correctStrategy
+			});
+
+			client.commit({
+				onSuccess: function() {
+					onSuccess(node, secret);
+				}
+			});
+			
+		};
+
+		return {
+			submitQuestion : qd.submitQuestion
+		};
+	};
+
+})(jQuery, AJ);
+
 // <!-- one.end -->
